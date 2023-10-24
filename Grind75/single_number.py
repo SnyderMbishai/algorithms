0287dc23758f547c79179445576810b8ae66e42a
snyder1 <snyder@poweredbypeople.com>
2023-10-24 14:19:46 +0300

    single number

---
+++ Grind75/single_number.py
@@ -0,0 +1,54 @@
+"""
+136. Single Number
+Solved
+Easy
+Topics
+Companies
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 10^4 <= nums[i] <= 3 * 10^4
+Each element in the array appears twice except for one element which appears only once.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    # frst try
+    def singleNumber(self, nums: List[int]) -> int:
+        checked = []
+        for num in nums:
+            if num in checked:
+                checked.remove(num)
+            else:
+                checked.append(num)
+        return checked[0]
+
+    # better:  bit manipulation
+    def singleNumber1(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res = res ^ num
+        return res
